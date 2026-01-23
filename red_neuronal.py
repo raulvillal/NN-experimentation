d2b10f28311d6bec765b204242074ed728e18a6e
Raúl Villalobos <raul.villalobosvega@ucr.ac.cr>
2026-01-23 04:28:29 -0600

    Añadiendo Binary Cross Entropy, así como la activación de sigmoid para la capa de salida junto con todas las debidas implementaciones para backpropagation en cada caso. Además se muestra un ejemplo del entrenamiento y validación de la red neuronal

--- red_neuronal.py
+++ red_neuronal.py
@@ -268,40 +268,82 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
         self.dinputs[range(samples), y_true] -= 1
         self.dinputs = self.dinputs / samples
 
+class Activation_Sigmoid:
+    def forward(self, inputs):
+        self.inputs = inputs
+        #Función sigmooid
+        self.output = 1 / (1 + np.exp(-inputs))
 
+    def backward(self, dvalues):
+        #Deivada de sigmoid para backpropagation
+        self.dinputs = dvalues * (self.output * (1 - self.output))
 
-X, y = spiral_data(samples=1000, classes=3)
+class Loss_BinaryCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if y_pred_clipped.ndim == 1:
+            y_pred_clipped = y_pred_clipped.reshape(-1, 1)
+        if y_true.ndim == 1:
+            y_true = y_true.reshape(-1, 1)
+
+        sample_losses = -(y_true * np.log(y_pred_clipped) + (1 - y_true) * np.log(1 - y_pred_clipped))
+        sample_losses = np.mean(sample_losses, axis=-1)
+        return sample_losses
+
+    def backward(self, dvalues, y_true):
+
+        dvalues = np.clip(dvalues, 1e-7, 1 - 1e-7)
+
+        # fuerza 2D coherente: (N,C)
+        if dvalues.ndim == 1:
+            dvalues = dvalues.reshape(-1, 1)
+        if y_true.ndim == 1:
+            y_true = y_true.reshape(-1, 1)
+
+        samples = dvalues.shape[0]
+        outputs = dvalues.shape[1]  # <- general (C)
+
+        self.dinputs = -(y_true / dvalues - (1 - y_true) / (1 - dvalues))
+        self.dinputs = self.dinputs / outputs
+        self.dinputs = self.dinputs / samples
+
+X, y = spiral_data(samples=100, classes=2)
+
+y = y.reshape(-1, 1)
 
 dense1 = Layer_Dense(2,64, weight_regularizer_l2=5e-4, bias_regularizer_l2=5e-4)
+
 activation1 = Activation_ReLU()
 
 dropout1 = Layer_Dropout(0.1)
 
-dense2 = Layer_Dense(64, 3)
+dense2 = Layer_Dense(64, 1)
 
-loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
+activation2 = Activation_Sigmoid()
 
-optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-7)
+loss_function = Loss_BinaryCrossentropy()
+
+optimizer = Optimizer_Adam(decay=5e-7)
 
 for epoch in range(10001):
 
     dense1.forward(X)
+
     activation1.forward(dense1.output)
 
-    dropout1.forwards(activation1.output)
+    dense2.forward(activation1.output)
 
-    dense2.forward(dropout1.output)
+    activation2.forward(dense2.output)
 
-    data_loss = loss_activation.forward(dense2.output, y)
+    data_loss = loss_function.calculate(activation2.output, y)
 
-    regularization_loss = loss_activation.loss.regularization_loss(dense1) + loss_activation.loss.regularization_loss(dense2)
+    regularization_loss = loss_function.regularization_loss(dense1) + loss_function.regularization_loss(dense2)
 
     loss = data_loss + regularization_loss
 
-    predictions = np.argmax(loss_activation.output, axis=1)
+    predictions = (activation2.output > 0.5)*1
 
-    if len(y.shape) == 2:
-        y = np.argmax(y, axis=1)
     accuracy = np.mean(predictions==y)
 
     if not epoch % 100:
@@ -312,10 +354,10 @@ for epoch in range(10001):
               f'reg_loss: {regularization_loss:.3f}), ' +
               f'lr: {optimizer.current_learning_rate}')
         
-    loss_activation.backward(loss_activation.output, y)
-    dense2.backward(loss_activation.dinputs)
-    dropout1.backward(dense2.dinputs)
-    activation1.backward(dropout1.dinputs)
+    loss_function.backward(activation2.output, y)
+    activation2.backward(loss_function.dinputs)
+    dense2.backward(activation2.dinputs)
+    activation1.backward(dense2.dinputs)
     dense1.backward(activation1.dinputs)
 
     optimizer.pre_update_params()
@@ -323,7 +365,9 @@ for epoch in range(10001):
     optimizer.update_params(dense2)
     optimizer.post_update_params()
 
-X_test, y_test = spiral_data(samples=100, classes=3)
+X_test, y_test = spiral_data(samples=100, classes=2)
+
+y_test = y_test.reshape(-1, 1)
 
 dense1.forward(X_test)
 
@@ -331,12 +375,16 @@ activation1.forward(dense1.output)
 
 dense2.forward(activation1.output)
 
-loss = loss_activation.forward(dense2.output, y_test)
+activation2.forward(dense2.output)
 
-predictions = np.argmax(loss_activation.output, axis=1)
+loss = loss_function.calculate(activation2.output, y_test)
 
-if len(y_test.shape) == 2:
-    y_test = np.argmax(y_test, axis=1)
+predictions = (activation2.output > 0.5)*1
 accuracy = np.mean(predictions==y_test)
 
+#if len(y_test.shape) == 2:
+#    y_test = np.argmax(y_test, axis=1)
+#accuracy = np.mean(predictions==y_test)
+
 print(f'Validation_acc: {accuracy:.3f}, loss: {loss:.3f}')
+
